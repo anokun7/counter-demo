836620009e77cf1525ce057c0d2263a594e869e7
Kumar, Anoop (2) <Anoop.Kumar2@McKesson.com>
2017-05-02 03:25:48 -0400

    Using templates

--- src/demo.go
+++ src/demo.go
@@ -1,37 +1,54 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "net/http"
-    "github.com/garyburd/redigo/redis"
-//  "time"
+  "github.com/garyburd/redigo/redis"
+  "log"
+  "os"
+  "net/http"
+  "html/template"
 )
 
+type Hit struct {
+  Host string
+  Count int
+}
+
+type Data struct {
+  CurrentHost, Env string
+  Hits []Hit
+}
+
+var data Data
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    host := os.Getenv("HOSTNAME")
-    fmt.Fprintf(w, "<p>Hi there, from <b>%s</b>!", host)
-    c, err := redis.Dial("tcp", "db:6379")
-    if err != nil {
-      panic(err)
-    }
-    defer c.Close()
-    c.Do("INCR", host)
-    keys, _ := redis.Strings(c.Do("KEYS", "*"))
-    fmt.Fprintf(w, "<hr/>")
-    fmt.Fprintf(w, "<table style='width: 10em; border-collapse: collapse;'><tr><th style='border: 2px dotted green;'>Container</th><th style='padding: 5px; border: 2px dotted green;'>#</th></tr>")
-    for _, key := range keys {
-      value, _ := redis.Int(c.Do("GET", key))
-      fmt.Fprintf(w, "<tr><td style='border: 1px solid green;'>%s</td>",key)
-      fmt.Fprintf(w, "<td style='border: 1px solid green; text-align: center;'>%d</td></tr>",value)
-    }
-    fmt.Fprintf(w, "</table>")
-    env := os.Getenv("ENVIRONMENT")
-    fmt.Fprintf(w, "<div style='color: lightgray; font: bold 96px/1.5 Arial, sans-serif; text-align: center; vertical-align: middle; line-height: 100%%; float: right; width:85%%'>%s</div>", env)
-//  time.Sleep(300 * time.Millisecond)
+  var hits []Hit
+  host := os.Getenv("HOSTNAME")
+  env := os.Getenv("ENVIRONMENT")
+  c, err := redis.Dial("tcp", "db:6379")
+  if err != nil {
+    panic(err)
+  }
+  defer c.Close()
+  c.Do("INCR", host)
+  keys, _ := redis.Strings(c.Do("KEYS", "*"))
+  for _, key := range keys {
+    value, _ := redis.Int(c.Do("GET", key))
+    hit := Hit{key, value}
+    hits = append(hits, hit)
+  }
+  data := Data{host, env, hits}
+  t, err := template.ParseFiles("tmpl/demo.html")
+  if err != nil {
+    log.Fatal("Parsing error: ", err)
+    return
+  }
+  exeErr := t.Execute(w, data)
+  if exeErr != nil {
+    log.Fatal("Execute error: ", exeErr)
+  }
 }
 
 func main() {
-    http.HandleFunc("/demo", handler)
-    http.ListenAndServe(":8080", nil)
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
 }
