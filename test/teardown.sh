a8425f8fef83384bda9c4e66ba1a6aecdcf7c374
Anoop V Kumar <anoop@docker.com>
2017-07-22 02:49:24 -0400

    Added vanilla html page that is excluded from proxy redirect

--- test/teardown.sh
+++ test/teardown.sh
@@ -2,4 +2,6 @@
 docker stack rm OnBuild QA1 QA2 Prod
 echo -n "Waiting.."
 for i in {0..9}; do sleep 1 ; echo -n "." ; done
+docker ps
 docker volume rm OnBuild_data QA1_data QA2_data Prod_data
+docker ps
